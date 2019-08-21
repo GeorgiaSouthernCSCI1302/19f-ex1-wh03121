009a323a791766a2c386f918a0f96fb12b9eddb4
wh03121 <wh03121@CEIT2553214X001.ad.georgiasouthern.edu>
2019-08-21 16:34:37 -0400

    Changed first print statement in main function (line 6)

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -3,7 +3,7 @@ package assignment;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello my name is Will!");
 		
 		//add a statement to display your name in the console
 		
